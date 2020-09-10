18fc3e93dbafe3a56e89ed8885d0a6a08c120566
bebeto84 <msolervlc@gmail.com>
2020-09-11 01:06:36 +0200

    feat(styles): create the rest of variables

---
+++ styles/variables.styles.ts
@@ -0,0 +1,46 @@
+const CSS_COLORS = {
+  purple60: '#4c00c2',
+  purple90: '#32007e',
+  purple: '#3b058e',
+  background: '#e5e5e5',
+  white: '#ffffff',
+  grey50: '#798291',
+  grey70: '#444e5d',
+  grey90: '#1a212c',
+  green30: '#19ac51',
+  red30: '#fc484c',
+  turquoise30: '#01c9c7',
+  turquoise50: '#01c4c3',
+};
+
+const CSS_SPACINGS = {
+  x1: `4px`,
+  x2: `8px`,
+  x3: `16px`,
+  x4: `24px`,
+  x5: `32px`,
+  x6: `40px`,
+  x7: `48px`,
+  x8: `56px`,
+};
+
+const CSS_FONT_SIZES = {
+  x1: `10px`,
+  x2: `14px`,
+  x3: `16px`,
+  x4: `24px`,
+  x5: `30px`,
+};
+
+const CSS_BORDER_RADIUS = {
+  x1: `16px`,
+  x2: `24px`,
+  x3: `100px`,
+};
+
+export {
+  CSS_COLORS,
+  CSS_SPACINGS,
+  CSS_FONT_SIZES,
+  CSS_BORDER_RADIUS,
+};
